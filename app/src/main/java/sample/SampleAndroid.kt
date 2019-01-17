beae60c21b9fd7f78a9388ee9a0f92f94ca3e672
oleg <osemen@rebbix.com>
2019-01-17 15:32:27 +0200

    implemented firebase data storage on android

--- app/src/main/java/sample/SampleAndroid.kt
+++ app/src/main/java/sample/SampleAndroid.kt
@@ -2,11 +2,17 @@ package sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
+import sample.R.id.submitButton
 
 actual class Database {
+    var firebase: FirebaseDatabase? = null
     actual fun store(value: String) {
-        //todo
+        firebase?.let {
+            it.reference.child("android").push().setValue(value)
+        }
     }
 }
 
@@ -16,7 +22,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        FirebaseApp.initializeApp(this)
+
         val db = Database()
+        db.firebase = FirebaseDatabase.getInstance()
         val presenter = Presenter(db)
 
         submitButton.setOnClickListener {
