2bb0778089ff9571b91ced3822276a4294fc4850
oleg <osemen@rebbix.com>
2019-01-17 16:13:56 +0200

    template for firebase in xcode project

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -2,13 +2,27 @@ import UIKit
 import app
 
 class ViewController: UIViewController {
+    
+    var presenter: Presenter? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = Proxy().proxyHello()
+        
+        let db = Database()
+        //todo
+        presenter = Presenter(db: db)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var input: UITextField!
+    
+    @IBAction func submit(_ sender: Any) {
+        presenter?.onSubmitClick(value: input.text)
+        input.text = ""
+    }
+    
 }
