cda2d9d8162f692e7a1b023739ea0ae0beef4b0f
oleg <osemen@rebbix.com>
2019-01-17 16:02:24 +0200

    implemented firebase database storage on iOS

--- app/src/iosMain/kotlin/sample/SampleIos.kt
+++ app/src/iosMain/kotlin/sample/SampleIos.kt
@@ -1,7 +1,12 @@
 package sample
 
+import firebase.FIRDatabase
+
 actual class Database {
+    var firebase: FIRDatabase? = null
     actual fun store(value: String) {
-        // todo
+        firebase?.let {
+            it.reference().child("iOS").childByAutoId().setValue(value)
+        }
     }
 }
